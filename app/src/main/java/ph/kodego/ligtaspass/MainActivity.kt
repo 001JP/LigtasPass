2895bcbb3c3b2045f5f105e031cc47d3092bcb81
JALMONTE <jasonjhonalmonte@gmail.com>
2023-04-22 04:21:15 +0800

    update main activity layout and other layout

--- app/src/main/java/ph/kodego/ligtaspass/MainActivity.kt
+++ app/src/main/java/ph/kodego/ligtaspass/MainActivity.kt
@@ -1,11 +1,15 @@
 package ph.kodego.ligtaspass
 
+import android.R
+import android.app.Dialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import ph.kodego.ligtaspass.databinding.ActivityMainBinding
+import ph.kodego.ligtaspass.databinding.DialogViewPasswordBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +35,10 @@ class MainActivity : AppCompatActivity() {
 
             //Play cat
             binding.catLottie.playAnimation()
+        }
+
+        binding.historyButton.setOnClickListener{
+            showCustomDialogue().show()
         }
 
     }
@@ -72,5 +80,17 @@ class MainActivity : AppCompatActivity() {
         }while (generatedPassword.length <= passwordLength)
 
         return generatedPassword
+    }
+    private fun showCustomDialogue(): Dialog {
+        return this?.let {
+            val builder = AlertDialog.Builder(this)
+            val dialogViewPasswordBinding : DialogViewPasswordBinding =
+                DialogViewPasswordBinding.inflate(this.layoutInflater)
+
+            with(builder) {
+                setView(dialogViewPasswordBinding.root)
+                create()
+            }
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
