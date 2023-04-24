cca5ca8adafd7520e2f72bc4952354efcbf96b88
John Patrick <john@x001.dev>
2023-04-24 13:57:04 +0800

    Add close keyboard.

--- app/src/main/java/ph/kodego/ligtaspass/MainActivity.kt
+++ app/src/main/java/ph/kodego/ligtaspass/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -50,6 +51,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.generateButton.setOnClickListener {
+
+            //Close keyboard
+            binding.generatedPasswordEditText.clearFocus()
+            binding.searchBox.clearFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.root.windowToken, 0)
+
             val generatedPassword = generatePassword()
             binding.generatedPasswordEditText.setText(generatedPassword)
             Log.d("MainActivity", "Generated Password: $generatedPassword")
