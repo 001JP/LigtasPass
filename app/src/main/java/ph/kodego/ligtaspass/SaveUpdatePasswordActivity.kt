163de50775106068e85321d5f96554e8013015e7
John Patrick <john@x001.dev>
2023-04-24 11:44:24 +0800

    Implement encryption and decryption.

--- app/src/main/java/ph/kodego/ligtaspass/SaveUpdatePasswordActivity.kt
+++ app/src/main/java/ph/kodego/ligtaspass/SaveUpdatePasswordActivity.kt
@@ -2,6 +2,7 @@ package ph.kodego.ligtaspass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import kotlinx.coroutines.launch
@@ -9,6 +10,7 @@ import ph.kodego.ligtaspass.database.PasswordApp
 import ph.kodego.ligtaspass.database.PasswordDAO
 import ph.kodego.ligtaspass.database.PasswordEntity
 import ph.kodego.ligtaspass.databinding.ActivitySaveUpdatePasswordBinding
+import ph.kodego.ligtaspass.utils.Constants
 
 class SaveUpdatePasswordActivity : AppCompatActivity() {
 
@@ -28,8 +30,14 @@ class SaveUpdatePasswordActivity : AppCompatActivity() {
 
         mPasswordDao = (application as PasswordApp).db.passwordDao()
 
+        //Sample Encryption and Decryption
+        val encryptedMessage = Constants.encrypt(this, "hello world")
+        val decryptedMessage = Constants.decrypt(this, encryptedMessage)
+
+        Log.i("SavePasswordActivity", "Encrypted: $encryptedMessage")
+        Log.i("SavePasswordActivity", "Decrypted: $decryptedMessage")
         //Sample
-        val password = PasswordEntity(0, "Title", "test@email.com", "12345678")
+        val password = PasswordEntity(0, "Title", "test@email.com", Constants.encrypt(this, "12345678"))
         //Sample
        lifecycleScope.launch {
            mPasswordDao.insert(password)
--- app/src/main/java/ph/kodego/ligtaspass/utils/Constants.kt
+++ app/src/main/java/ph/kodego/ligtaspass/utils/Constants.kt
@@ -1,9 +1,40 @@
 package ph.kodego.ligtaspass.utils
 
+import android.content.Context
+import ph.kodego.ligtaspass.encryption.CryptoManager
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 object Constants {
     const val INCLUDE_SYMBOLS = "include_symbols"
     const val INCLUDE_NUMBERS = "include_numbers"
     const val INCLUDE_LOWERCASE = "include_lowercase"
     const val INCLUDE_UPPERCASE = "include_uppercase"
     const val PASSWORD_LENGTH = "password_length"
+
+    private const val ENCRYPTION_FILE = "secret.txt"
+    fun encrypt(context: Context, message: String): String {
+
+        val cryptoManager = CryptoManager()
+
+        val bytes = message.encodeToByteArray()
+        val file = File(context.filesDir, ENCRYPTION_FILE)
+        if (!file.exists()){
+            file.createNewFile()
+        }
+        val fos = FileOutputStream(file)
+
+        return cryptoManager.encrypt(bytes, fos).decodeToString()
+    }
+
+    fun decrypt(context: Context, message: String): String{
+
+        val cryptoManager = CryptoManager()
+
+        val fileDecrypt = File(context.filesDir, ENCRYPTION_FILE)
+        return cryptoManager.decrypt(
+            inputStream = FileInputStream(fileDecrypt)
+        ).decodeToString()
+    }
 }
