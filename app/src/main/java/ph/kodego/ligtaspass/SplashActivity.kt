1633e853659f1a930ee384b42bccea7ea84c1942
John Patrick <john@x001.dev>
2023-04-30 23:17:35 +0800

    Added fingerprint.

--- app/src/main/java/ph/kodego/ligtaspass/SplashActivity.kt
+++ app/src/main/java/ph/kodego/ligtaspass/SplashActivity.kt
@@ -1,9 +1,21 @@
 package ph.kodego.ligtaspass
 
+import android.Manifest
 import android.animation.Animator
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import ph.kodego.ligtaspass.databinding.ActivitySplashBinding
 
@@ -11,6 +23,27 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
+    private var cancellationSignal: CancellationSignal? = null
+
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() =
+            @RequiresApi(Build.VERSION_CODES.P)
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    toast("Authentication error.")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+                    //Fingerprint success, move to next activity
+                    toast("Authentication success!")
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+            }
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
@@ -18,14 +51,27 @@ class SplashActivity : AppCompatActivity() {
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.primary_red)
 
+        checkBiometricSupport()
+
+        binding.fingerprintImageView.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                val biometricPrompt = BiometricPrompt.Builder(this)
+                    .setTitle("Enter with your Biometrics")
+                    .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_WEAK or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                    .build()
+
+                biometricPrompt.authenticate(getCancellationSignal(), mainExecutor, authenticationCallback)
+            }
+        }
+
         binding.lottieAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator) {
 
             }
 
             override fun onAnimationEnd(animation: Animator) {
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                finish()
+                //startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                //finish()
             }
 
             override fun onAnimationCancel(animation: Animator) {
@@ -37,6 +83,28 @@ class SplashActivity : AppCompatActivity() {
             }
 
         })
+    }
 
+    private fun getCancellationSignal() :CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            toast("Authentication cancelled by user")
+        }
+
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport(): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED) {
+            toast("Fingerprint Authentication is not enabled")
+            return false
+        }
+
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)
+    }
+
+    private fun toast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
